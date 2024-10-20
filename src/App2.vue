983e05c4eeb22775769a394bf8e8005415d3c0a2
BurhanBudak <budak.burha@gmail.com>
2024-10-20 12:00:34 +0200

    first commit

---
+++ src/App2.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'John Doe',
+      status: 'active',
+      tasks: ['Task one', 'Task Two', 'Task Three'],
+      link: 'http://google.com'
+    };
+  },
+  methods: {
+    toggleStatus() {
+      if (this.status === 'active') {
+        
+        this.status = 'pending'
+      } else if (this.status === 'pending') {
+        ;
+        this.status = 'inactive'
+      } else {
+        
+        this.status = 'active'
+      }
+    },
+  },
+};
+</script>
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status == 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+
+  <a v-bind:href="link">Google with v-bind</a>
+  <br>
+  <a :href="link">Google without v-bind</a>
+  <br>
+
+  <button v-on:click="toggleStatus">Change Status</button>
+</template>
+
+<style scoped></style>
